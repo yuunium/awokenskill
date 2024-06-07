33c706e206a7ee3a5dd8e00598daa454c7608cce
yuunium <yyngtm0623@gmail.com>
2024-06-07 23:07:12 +0900

    canvas

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
+
 function box(a){
     document.getElementById(a).src='./pic/selected.png';
     document.getElementById('selected_box').innerHTML=a;
-  }
-  function set(a){
-    var in_box = document.getElementById('selected_box').innerHTML;
-    console.log(in_box);
-    document.getElementById(in_box).src='./pic/'+a+'.png';
-    document.getElementById('selected_box').innerHTML='';
-    in_box='';
+};
+
+function set(a){
+  var in_box = document.getElementById('selected_box').innerHTML;
+  console.log(in_box);
+  document.getElementById(in_box).src='./pic/'+a+'.png';
+  document.getElementById('selected_box').innerHTML='';
+  in_box='';
+};
+
+function color(a){
+  document.getElementById('body').style.backgroundColor= a;
+}
+
+function downloadPng() {
+  const element =document.getElementById('canvas');
+  const getImage = document.getElementById("getImage");
+  html2canvas(element).then(canvas => {
+    getImage.setAttribute("href", canvas.toDataURL());
+    getImage.setAttribute("download", "awokenskill.png");
+    getImage.click();
+  });	
   };
-  function color(a){
-    document.getElementById('body').style.backgroundColor= a;
-  }
