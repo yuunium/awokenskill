85d603a986e2b42ab2ce16fc0c714c011a5af90b
yuunium <yyngtm0623@gmail.com>
2024-08-13 17:50:01 +0900

    url

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-
-function box(a){
-    document.getElementById(a).src='./pic/selected.png';
-    document.getElementById('selected_box').innerHTML=a;
+function box(a) {
+  document.getElementById(a).src = './pic/selected.png';
+  document.getElementById('selected_box').innerHTML = a;
 };
 
-function set(a){
+function set(a) {
   var in_box = document.getElementById('selected_box').innerHTML;
-  document.getElementById(in_box).src='./pic/'+a+'.png';
-  document.getElementById('selected_box').innerHTML='';
-  in_box='';
+  document.getElementById(in_box).src = './pic/' + a + '.png';
+  document.getElementById('selected_box').innerHTML = '';
 };
 
 function downloadPngFile() {
-  const element =document.getElementById('canvas');
+  const element = document.getElementById('canvas');
   const getImage = document.getElementById("downloadImage");
-  html2canvas(element,{backgroundColor: null,scale: 3,height:800}).then(canvas => {
+  html2canvas(element, { backgroundColor: null, scale: 3, height: 800 }).then(canvas => {
     getImage.setAttribute("href", canvas.toDataURL());
     getImage.setAttribute("download", "awokenskill.png");
     getImage.click();
@@ -22,38 +20,77 @@ function downloadPngFile() {
 };
 
 function downloadPngDisplay() {
-  const element =document.getElementById('canvas');
+  const element = document.getElementById('canvas');
   const getImage = document.getElementById("displayImage");
-  html2canvas(element,{backgroundColor: null,scale: 3,height:800}).then(canvas => {
+  html2canvas(element, { backgroundColor: null, scale: 3, height: 800 }).then(canvas => {
     const imageData = canvas.toDataURL("image/png");
     getImage.src = imageData;
   });
-  getImage.style.display="block";
+  getImage.style.display = "block";
 };
 
-function nomal(){
+function nomal() {
   document.getElementById('super').style.display = "none";
-  for(let i = 1;i<10;i++){
-    var getbox = document.getElementById('box'+i);
-    getbox.src='./pic/space.png';
+  for (let i = 1; i < 10; i++) {
+    var getbox = document.getElementById('box' + i);
+    getbox.src = './pic/space.png';
     getbox.style.display = "";
   };
 };
 
-function super_skill(){
+function super_skill() {
   document.getElementById('box1').src = './pic/space.png';
   document.getElementById('super').style.display = "";
-  for(let i = 2;i<10;i++){
-    var getbox = document.getElementById('box'+i);
+  for (let i = 2; i < 10; i++) {
+    var getbox = document.getElementById('box' + i);
     getbox.style.display = "none";
   };
 };
 
-function syncro(){
+function syncro() {
   document.getElementById('box1').src = './pic/syncro.png';
   document.getElementById('super').style.display = "none";
-  for(let i = 2;i<10;i++){
-    var getbox = document.getElementById('box'+i);
+  for (let i = 2; i < 10; i++) {
+    var getbox = document.getElementById('box' + i);
     getbox.style.display = "none";
   };
 };
+
+function even(array) {
+  const result = [];
+
+  array.forEach((element, index) => {
+    if (index % 2 === 0) {
+      result.push(element);
+    }
+  });
+
+  return result;
+}
+
+function odd(array) {
+  const result = [];
+
+  array.forEach((element, index) => {
+    if (index % 2 !== 0) {
+      result.push(element);
+    }
+  });
+
+  return result;
+}
+
+var params = location.href.split("?");
+console.log(params);
+if (params.length > 1) {
+  let X = params[1].split(/&|=/);
+  console.log(X);
+  let Y = even(X);
+  let Z = odd(X);
+  let length = Y.length;
+
+  for (let i = 0; i < length; i++) {
+    document.getElementById('selected_box').innerHTML = "box" + String(Number(Y[i]));
+    set(Z[i]);
+  }
+}
