a04ff7177e576e901f95fd899e7a6e1e85dcf74a
yuunium <yyngtm0623@gmail.com>
2024-08-13 12:49:29 +0900

    service

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+navigator.serviceWorker.getRegistrations().then(function(registrations) {
+  for(let registration of registrations) {
+    registration.unregister();
+  }
+});
+caches.keys().then(function(keys) {
+  let promises = [];
+  keys.forEach(function(cacheName) {
+    if (cacheName) {
+      promises.push(caches.delete(cacheName));
+    }
+  });
+});
