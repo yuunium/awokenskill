3477ee26f27b7b6d668a6c5cb39bebebc964c4ce
yuunium <yyngtm0623@gmail.com>
2024-08-13 14:46:32 +0900

    download

--- script.js
+++ script.js
@@ -11,14 +11,24 @@ function set(a){
   in_box='';
 };
 
-function downloadPng() {
+function downloadPngFile() {
   const element =document.getElementById('canvas');
-  const getImage = document.getElementById("getImage");
+  const getImage = document.getElementById("downloadImage");
   html2canvas(element,{backgroundColor: null,scale: 3,height:800}).then(canvas => {
     getImage.setAttribute("href", canvas.toDataURL());
     getImage.setAttribute("download", "awokenskill.png");
     getImage.click();
   });
+};
+
+function downloadPngDisplay() {
+  const element =document.getElementById('canvas');
+  const getImage = document.getElementById("displayImage");
+  html2canvas(element,{backgroundColor: null,scale: 3,height:800}).then(canvas => {
+    const imageData = canvas.toDataURL("image/png");
+    getImage.src = imageData;
+  });
+  getImage.style.display="block";
 };
 
 function nomal(){
