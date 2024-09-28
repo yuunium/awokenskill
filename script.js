0fd6e4fb937b255d214a21f0070e07bd4668505e
yuunium <yyngtm0623@gmail.com>
2024-09-29 00:55:09 +0900

    copy

--- script.js
+++ script.js
@@ -57,6 +57,7 @@ function copy() {
   console.log(boxes);
   navigator.clipboard.writeText(now_url);
   console.log(now_url);
+  document.getElementById("copy").innerHTML = "コピーしました！"
 };
 
 function skillreset() {
